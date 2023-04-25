e3f63e0267a51ec9733826a0c7419771285a54a0
Marily-fajardo <fajardomarily90@gmail.com>
2023-04-25 08:33:33 -1200

    fin

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { CssBaseline, Typography } from "@mui/material";
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
+import { Products } from "./pages/Products";
 
 function App() {
   const [products, setProducts] = useState([]);
@@ -28,6 +29,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
+        <Route path="products" element={<Products />} />
       </Routes>
     </div>
   );
